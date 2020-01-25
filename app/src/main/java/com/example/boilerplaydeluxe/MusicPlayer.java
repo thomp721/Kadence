e7915c82968089a4474feefbb7e42d3403ed225b
nihalr13 <58914812+nihalr13@users.noreply.github.com>
2020-01-25 13:11:45 -0500

    Player media player + inst vars/constructors

--- app/src/main/java/com/example/boilerplaydeluxe/MusicPlayer.java
+++ app/src/main/java/com/example/boilerplaydeluxe/MusicPlayer.java
@@ -1,14 +1,32 @@
 package com.example.boilerplaydeluxe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
-public class MusicPlayer extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Scanner;
+import java.io.IOException;
 
+public class MusicPlayer extends AppCompatActivity {
+    Scanner s = new Scanner(System.in);
+    private double timeRemaining;
+    private int[] soundtrack;
+    public MusicPlayer(int[] soundtrack) {
+        this.soundtrack = soundtrack;
+        System.out.println("Enter a number of minutes you would like to listen to music for");
+        timeRemaining = s.nextDouble();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_music_player);
+        setContentView(R.layout.activity_main);
+        MediaPlayer mp = new MediaPlayer();
+        try {
+            mp.setDataSource("/sdcard/Music/maine.mp3");
+            timeRemaining = timeRemaining - mp.getDuration();
+
+        } catch(IOException io) {
+
+        }
     }
 }
