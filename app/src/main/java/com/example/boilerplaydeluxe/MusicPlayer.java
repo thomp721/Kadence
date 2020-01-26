7452b132123650489ea80ef389beba6dfce45b1a
Jake Visniski <visjake@live.com>
2020-01-26 08:25:38 -0500

    This is the last commit for the hackathon. It includes a prototype UI for the player.

--- app/src/main/java/com/example/boilerplaydeluxe/MusicPlayer.java
+++ app/src/main/java/com/example/boilerplaydeluxe/MusicPlayer.java
@@ -14,32 +14,11 @@ public class MusicPlayer extends AppCompatActivity {
 
     private double timeRemaining;
     private com.example.Library.Soundtrack[] soundtrack;
-    public MusicPlayer(com.example.Library.Soundtrack[] soundtrack) {
-        this.soundtrack = soundtrack;
 
-        onCreate(new Bundle());
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MediaPlayer mp = new MediaPlayer();
-        while(timeRemaining > 0) {
-            for (int i = 0; i < soundtrack.length; i++) {
-                MusicPlayerSetup mps = new MusicPlayerSetup(soundtrack[i].getHeartRate());
-                try {
-                    com.example.Library.DbRetrieve dbr = new com.example.Library.DbRetrieve();
-                    mp.setDataSource(dbr.retrieveSongUID(soundtrack[i].getGenre(), mps.getMusicLevel(),
-                            (int) (Math.random() * 5 + 1)));
-                    timeRemaining = timeRemaining - soundtrack[i].getTime();
-                    if (timeRemaining <= 0) {
-                        break;
-                    }
 
-                } catch (IOException io) {
-
-                }
-            }
-        }
     }
 }
